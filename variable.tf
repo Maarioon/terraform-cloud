4561f3190e30e73be8b35c1e2d1a0eebace2549f
Marioon <luwamarielleky@gmail.com>
2025-02-01 03:06:48 -0800

    u

--- variable.tf
+++ variable.tf
@@ -1,6 +1,14 @@
+# variable "region" {
+#   default = "us-east-1"
+# }
+
 variable "region" {
-  default = "us-east-1"
+  description = "AWS region"
+  type        = string
+  default     = "us-east-1"
 }
+
+
 
 variable "vpc_cidr" {
   default = "10.0.0.0/16"
