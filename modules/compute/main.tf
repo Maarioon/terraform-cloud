5e01ed2a87337162afdf04bacc8a8bb240f28319
Marioon <luwamarielleky@gmail.com>
2025-02-02 15:35:36 -0800

    i

--- modules/compute/main.tf
+++ modules/compute/main.tf
@@ -1,54 +1,54 @@
-# create instance for jenkins
-resource "aws_instance" "Jenkins" {
-  ami                         = var.ami-jenkins
-  instance_type               = "t2.small"
-  subnet_id                   = var.subnets-compute
-  vpc_security_group_ids      = var.sg-compute
-  associate_public_ip_address = true
-  key_name                    = var.keypair
+# # create instance for jenkins
+# resource "aws_instance" "Jenkins" {
+#   ami                         = var.ami-jenkins
+#   instance_type               = "t2.small"
+#   subnet_id                   = var.subnets-compute
+#   vpc_security_group_ids      = var.sg-compute
+#   associate_public_ip_address = true
+#   key_name                    = var.keypair
 
-  tags = merge(
-    var.tags,
-    {
-      Name = "techeon-Jenkins"
-    },
-  )
-}
+#   tags = merge(
+#     var.tags,
+#     {
+#       Name = "techeon-Jenkins"
+#     },
+#   )
+# }
 
 
-# create instance for sonbarqube
-resource "aws_instance" "sonarqube" {
-  ami                         = var.ami-sonar
-  instance_type               = "t2.medium"
-  subnet_id                   = var.subnets-compute
-  vpc_security_group_ids      = var.sg-compute
-  associate_public_ip_address = true
-  key_name                    = var.keypair
+# # create instance for sonbarqube
+# resource "aws_instance" "sonarqube" {
+#   ami                         = var.ami-sonar
+#   instance_type               = "t2.medium"
+#   subnet_id                   = var.subnets-compute
+#   vpc_security_group_ids      = var.sg-compute
+#   associate_public_ip_address = true
+#   key_name                    = var.keypair
 
-  tags = merge(
-    var.tags,
-    {
-      Name = "techeon-sonbarqube"
-    },
-  )
-}
+#   tags = merge(
+#     var.tags,
+#     {
+#       Name = "techeon-sonbarqube"
+#     },
+#   )
+# }
 
-# create instance for artifactory
-resource "aws_instance" "artifactory" {
-  ami                         = var.ami-jfrog
-  instance_type               = "t2.medium"
-  subnet_id                   = var.subnets-compute
-  vpc_security_group_ids      = var.sg-compute
-  associate_public_ip_address = true
-  key_name                    = var.keypair
+# # create instance for artifactory
+# resource "aws_instance" "artifactory" {
+#   ami                         = var.ami-jfrog
+#   instance_type               = "t2.medium"
+#   subnet_id                   = var.subnets-compute
+#   vpc_security_group_ids      = var.sg-compute
+#   associate_public_ip_address = true
+#   key_name                    = var.keypair
 
-  tags = merge(
-    var.tags,
-    {
-      Name = "techeon-artifactory"
-    },
-  )
-}
+#   tags = merge(
+#     var.tags,
+#     {
+#       Name = "techeon-artifactory"
+#     },
+#   )
+# }
 
 
 
