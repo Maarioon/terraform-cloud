614d610db924497253a859d54262ad47745a2e8f
Marioon <luwamarielleky@gmail.com>
2025-02-03 08:46:56 -0800

    i

--- modules/ALB/alb.tf
+++ modules/ALB/alb.tf
@@ -34,17 +34,17 @@ resource "aws_lb_target_group" "nginx-tgt" {
   vpc_id      = var.vpc_id
 }
 
-resource "aws_lb_listener" "nginx-listner" {
-  load_balancer_arn = aws_lb.ext-alb.arn
-  port              = 443
-  protocol          = "HTTPS"
-  certificate_arn   = aws_acm_certificate_validation.oyindamola.certificate_arn
+# resource "aws_lb_listener" "nginx-listner" {
+#   load_balancer_arn = aws_lb.ext-alb.arn
+#   port              = 443
+#   protocol          = "HTTPS"
+#   certificate_arn   = aws_acm_certificate_validation.oyindamola.certificate_arn
 
-  default_action {
-    type             = "forward"
-    target_group_arn = aws_lb_target_group.nginx-tgt.arn
-  }
-}
+#   default_action {
+#     type             = "forward"
+#     target_group_arn = aws_lb_target_group.nginx-tgt.arn
+#   }
+# }
 
 # ----------------------------
 #Internal Load Balancers for webservers
@@ -111,37 +111,55 @@ resource "aws_lb_target_group" "tooling-tgt" {
   vpc_id      = var.vpc_id
 }
 
+
+# resource "aws_lb_target_group" "nginx-tgt" {
+#   name     = "nginx-tgt"
+#   port     = 80
+#   protocol = "HTTP"
+#   vpc_id   = var.vpc_id  # Replace with actual VPC ID
+# }
+
 # For this aspect a single listener was created for the wordpress which is default,
 # A rule was created to route traffic to tooling when the host header changes
 
 
-resource "aws_lb_listener" "web-listener" {
-  load_balancer_arn = aws_lb.ialb.arn
-  port              = 443
-  protocol          = "HTTPS"
-  certificate_arn   = aws_acm_certificate_validation.oyindamola.certificate_arn
+# resource "aws_lb_listener" "web-listener" {
+#   load_balancer_arn = aws_lb.ialb.arn
+#   port              = 443
+#   protocol          = "HTTPS"
+#   certificate_arn   = aws_acm_certificate_validation.oyindamola.certificate_arn
 
 
-  default_action {
-    type             = "forward"
-    target_group_arn = aws_lb_target_group.wordpress-tgt.arn
-  }
-}
+#   default_action {
+#     type             = "forward"
+#     target_group_arn = aws_lb_target_group.wordpress-tgt.arn
+#   }
+# }
 
 # listener rule for tooling target
 
-resource "aws_lb_listener_rule" "tooling-listener" {
-  listener_arn = aws_lb_listener.web-listener.arn
-  priority     = 99
+# resource "aws_lb_listener_rule" "tooling-listener" {
+#   listener_arn = aws_lb_listener.web-listener.arn
+#   priority     = 99
 
-  action {
-    type             = "forward"
-    target_group_arn = aws_lb_target_group.tooling-tgt.arn
-  }
+#   action {
+#     type             = "forward"
+#     target_group_arn = aws_lb_target_group.tooling-tgt.arn
+#   }
 
-  condition {
-    host_header {
-      values = ["tooling.kadara-eda.shop"]
-    }
-  }
-}
+#   condition {
+#     host_header {
+#       values = ["tooling.kadara-eda.shop"]
+#     }
+#   }
+# }
+
+# resource "aws_lb" "ext-alb" {
+#   name               = "techeon-ext-alb"
+#   internal           = false
+#   load_balancer_type = "application"
+#   security_groups    = [var.public-sg]  # Replace with actual security group
+#   subnets           = [var.public-sbn-1, var.public-sbn-2,]  # Replace with actual subnets
+
+#   enable_deletion_protection = false
+# }
